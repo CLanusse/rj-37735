62b9e8b881fba8031c243dcc74937ec792fd658c
Conrado Lanusse <conrado.lanusse@gmail.com>
2022-05-12 20:49:53 -0300

    Clase 1 - instalacion

--- src/App.js
+++ src/App.js
@@ -2,20 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Conrado Lanusse - the best</h1>
+
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+        <ul>
+          <h3>TUTORES</h3>
+          <li>Gaston</li>
+          <li>Sergio</li>
+          <li>Armando</li>
+          <li>Anthony</li>
+        </ul>
+
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://coderhouse.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Go Coder!
         </a>
       </header>
     </div>
