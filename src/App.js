03a0e261d719334a2eadb63bcfe757dccc65804a
Conrado Lanusse <conrado.lanusse@gmail.com>
2022-05-26 19:57:46 -0300

    Clase 5 - 1

--- src/App.js
+++ src/App.js
@@ -2,34 +2,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css'
 import {Navbar} from './components/Navbar/Navbar'
 import { ItemListContainer } from './components/ItemListContainer/ItemListContainer'
-import Counter from './ejemplos/Counter/Counter';
-import Counter2 from './ejemplos/Counter2/Counter2';
-import { useState } from 'react';
 
 
 
 function App() {
 
-  const [mostrar, setMostrar] = useState(true)
-  
-  const mostrarCounter = () => {
-    setMostrar(!mostrar)
-  }
-
   return (
     <div>
         <Navbar/>
 
-        <button className='btn btn-primary' onClick={mostrarCounter}>MOSTRAR/OCULTAR</button>
-
-        {
-          mostrar ? <Counter/> : <div></div>
-        }
-
-        
-        {/* <Counter2/> */}
-
-        {/* <ItemListContainer/> */}
+        <ItemListContainer/>
         
     </div>
   );
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,15 +1,72 @@
 
+const productos = [
+    {
+        id: 1,
+        nombre: "Producto 1",
+        desc: "Una descripción",
+        precio: 1200,
+        img: 'https://via.placeholder.com/220'
+    },
+    {
+        id: 2,
+        nombre: "Producto 2",
+        desc: "Una descripción",
+        precio: 2200,
+        img: 'https://via.placeholder.com/220'
+    },
+    {
+        id: 3,
+        nombre: "Producto 3",
+        desc: "Una descripción",
+        precio: 3200,
+        img: 'https://via.placeholder.com/220'
+    },
+    {
+        id: 4,
+        nombre: "Producto 4",
+        desc: "Una descripción",
+        precio: 4200,
+        img: 'https://via.placeholder.com/220'
+    },
+    {
+        id: 5,
+        nombre: "Producto 5",
+        desc: "Una descripción",
+        precio: 5200,
+        img: 'https://via.placeholder.com/220'
+    },
+]
+
+
+export const ItemListContainer = () => {
+
+
+    const pedirDatos = () => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                    resolve(productos)
+            }, 2000)
+        })
+    }
+
+    pedirDatos() //fetch
+        .then((resp) => {
+            console.log(resp)
+        })
+        .catch((error) => {
+            console.log('ERROR', error)
+        })
 
 
 
-export const ItemListContainer = ({nombre}) => {
+    
 
     return (
         <section className="container my-5">
             <h2>Nuestro productos</h2>
             <hr/>
 
-            <p>Bienvenido {nombre}</p>
+            
 
         </section>
     )
